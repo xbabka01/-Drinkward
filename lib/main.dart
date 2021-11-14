a10c643bdff8e15df50985322671f2057ccbee72
michaldurina <miso.durina@gmail.com>
2021-11-14 20:37:54 +0100

    New home screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Drinkward',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Drinkward'),
     );
   }
 }
@@ -67,47 +67,48 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+              bottom: const TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.calendar_today_rounded)),
+                  Tab(icon: Icon(Icons.house_rounded )),
+                  Tab(icon: Icon(Icons.map_rounded )),
+                ],
+              ),
+              title: Row(children: <Widget>[
+                Container(
+                    padding: EdgeInsets.symmetric(horizontal: 3),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20)),
+                    constraints: BoxConstraints(maxWidth: 300),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.search),
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 5, vertical: 1),
+                      ),
+                    )),
+                new IconButton(
+                  icon: const Icon(Icons.person_rounded),
+                  alignment: Alignment.centerRight,
+                  tooltip: 'Person profile',
+                  onPressed: () {},
+                ),
+              ])),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.calendar_today_rounded),
+              Icon(Icons.house_rounded ),
+              Icon(Icons.map_rounded ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
