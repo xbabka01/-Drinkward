c9499b69b46789cdc8dc9cd26b424f380b094ad3
Radovan Klembara <radovan@klembara.pro>
2021-11-16 00:36:38 +0100

    initial event detail implementation

--- lib/EventCardView.dart
+++ lib/EventCardView.dart
@@ -1,3 +1,4 @@
+import 'package:drinkward/EventDetail.dart';
 import 'package:drinkward/register.dart';
 import 'package:drinkward/widget/common.dart';
 import 'package:flutter/material.dart';
@@ -22,56 +23,67 @@ class _EventCardView extends State<EventCardView> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            ListTile(
-              leading: Icon(
-                Icons.local_drink_sharp,
-                color: Colors.blue,
-                size: 40.0,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => EventDetail()),
+          );
+        },
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(
+                  Icons.local_drink_sharp,
+                  color: Colors.blue,
+                  size: 40.0,
+                ),
+                title: Text(
+                  'Finlandia 2+1',
+                  style: heading2.copyWith(color: textBlack),
+                ),
+                subtitle: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Icon(Icons.calendar_today_sharp),
+                        Text("21.8.2020 - 28.8.2020"),
+                        const SizedBox(width: 8),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Icon(Icons.location_on_sharp),
+                        Text("Hospoda u Karly, Brno"),
+                        const SizedBox(width: 8),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Icon(
+                          Icons.thumb_up_off_alt_outlined,
+                          color: Colors.green,
+                        ),
+                        Text("56x"),
+                        const SizedBox(width: 8),
+                        Icon(
+                          Icons.thumb_down_off_alt_outlined,
+                          color: Colors.red,
+                        ),
+                        Text("20x"),
+                        const SizedBox(width: 8),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              title: Text('Finlandia 2+1'),
-              subtitle: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(Icons.calendar_today_sharp),
-                      Text("21.8.2020 - 28.8.2020"),
-                      const SizedBox(width: 8),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(Icons.location_on_sharp),
-                      Text("Hospoda u Karly, Brno"),
-                      const SizedBox(width: 8),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(
-                        Icons.thumb_up_off_alt_outlined,
-                        color: Colors.green,
-                      ),
-                      Text("56x"),
-                      const SizedBox(width: 8),
-                      Icon(
-                        Icons.thumb_down_off_alt_outlined,
-                        color: Colors.red,
-                      ),
-                      Text("20x"),
-                      const SizedBox(width: 8),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
