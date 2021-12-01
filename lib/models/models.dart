3a24672cc4c9b462e38f69b38eae3f56e2f52e19
Martina Chripková <chripmat@gmail.com>
2021-12-01 01:50:05 +0100

    Added database models

---
+++ lib/models/models.dart
@@ -0,0 +1,26 @@
+class User {
+  int id;
+  String email;
+  String password;
+  int karma;
+}
+
+class Event {
+  int id;
+  DateTime startDate;
+  DateTime endDate;
+  String description;
+}
+
+class Pub {
+  int id;
+  String name;
+  int google_id;
+  String city;
+  String street;
+  int street_nunmber;
+}
+
+class Rated {
+  int value;
+}
