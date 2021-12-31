65e01e5db3b2e7c3bff8b94aca7734e4817f8f98
Peter Babka <xbabka01@stud.fit.vutbr.cz>
2021-12-31 17:54:30 +0100

    Fix bug

--- lib/EventsListView.dart
+++ lib/EventsListView.dart
@@ -12,15 +12,14 @@ class EventsListView extends StatefulWidget {
 
 class _EventsListView extends State<EventsListView> {
 
-  var connection = new PostgreSQLConnection("ec2-52-209-246-87.eu-west-1.compute.amazonaws.com",
-      5432,
-      "d9o5rtu028946m",
-      username: "cohzowecdgnnae",
-      password: "8b02dd03e907d484f22e131a74b40c4d087cdc4a50f9f22bee4d02c6506e285d",
-      useSSL: true
-  );
-
   Future<List> getAllRecords() async {
+    var connection = PostgreSQLConnection("ec2-52-209-246-87.eu-west-1.compute.amazonaws.com",
+        5432,
+        "d9o5rtu028946m",
+        username: "cohzowecdgnnae",
+        password: "8b02dd03e907d484f22e131a74b40c4d087cdc4a50f9f22bee4d02c6506e285d",
+        useSSL: true
+    );
     await connection.open();
     results = await connection.query("SELECT * FROM public.\"Events\"");
     await connection.close();
